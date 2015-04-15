d3c683ca107b7f8b2405a968a980827c4a769f68
Lou <tuntun0847@gmail.com>
2015-04-15 23:22:17 +0800

    Simple Wifi complete
            send hard-coded message to server using POST
            only tested in local network but should work fine for abtirary
            IP

--- exp2/wifi/wifi.js
+++ exp2/wifi/wifi.js
@@ -4,21 +4,29 @@ var statusCode = 200;
 var count = 1;
 
 var opt = {
-    hostname: '192.168.2.105',
-    port: '1234',
+    hostname: '192.168.2.106',
+    port: 1234,
     path: '/test',
     method: 'POST'
-}
-var data = {
-    item1: '1',
-    item2: '2'
-}
+};
+var data = JSON.stringify({
+    'item1' : '1',
+    'item2' : '2'
+});
 setImmediate(function start () {
   console.log('http request #' + (count++))
   request = http.request(opt, function (res) {
       console.log('# statusCode', res.statusCode);
+      res.on('close', function(){
+        console.log('done');
+        setImmediate(start)});
   });
 
+  request.on('error', function(e){
+    console.log('error!' + e.message)
+    console.log(e)
+  })
   request.write(data);
   request.end();
 });
+
