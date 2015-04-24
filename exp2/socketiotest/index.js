f5c7b81aaaca140a378cb78e3f3d623a9dfaee61
weiling <weiling0207@gmail.com>
2015-04-24 10:10:58 +0800

    add photo
    	not complete

---
+++ exp2/socketiotest/index.js
@@ -0,0 +1,65 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+app.get('/', function(req, res){
+  res.sendFile('/Users/ling/Documents/eslab/exp2/socketiotest/index.html');
+});
+
+/*app.get('/pic', function(req, res){
+  setInterval(function(){
+  	var x = 1;
+	res.sendFile('/Users/ling/Documents/eslab/exp2/socketiotest/00'+x+'.jpg'); 
+	x++;
+  }, 1000);
+});//傳了以後不能再傳
+*/
+
+app.get('/d3', function(req, res){
+  res.sendFile('/Users/ling/Documents/eslab/exp2/socketiotest/d3.v3.min.js');
+});
+
+app.get('/socket', function(req, res){
+  res.sendFile('/Users/ling/Documents/eslab/exp2/socketiotest/socket.io-1.2.0.js');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.emit('newphoto');
+  socket.on('photo', function(){
+	var imgname = '/Users/ling/Documents/eslab/exp2/socketiotest/001.jpg';
+//	console.log("收到要photo的emit了");
+	fs.readFile(imgname, 'utf8', function(err, img){
+		if (err) throw err;
+		socket.emit('image', {content: img});
+
+    });
+  });
+
+  var a=100;
+  setInterval(
+    function(){  
+
+	socket.emit('update', {data: [{'x':a, 'y':a}, {'x':0, 'y':0}] } );
+	a+=10;
+
+    }, 100)
+
+  setInterval(
+    function(){
+
+	socket.emit('acc',{data_acc: [{'x':a, 'y':a}, {'x':0, 'y':0}] });
+   	a+=10;
+
+    }, 100)	
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+
