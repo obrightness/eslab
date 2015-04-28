86867b59dbe79d61997a4930b1973271e44b8fbd
weiling <weiling0207@gmail.com>
2015-04-28 21:51:49 +0800

    socket.io v3
       delete redundant code

--- exp2/socketiotest/index.js
+++ exp2/socketiotest/index.js
@@ -7,14 +7,6 @@ app.get('/', function(req, res){
   res.sendFile('/Users/ling/Documents/eslab/exp2/socketiotest/index.html');
 });
 
-/*app.get('/pic', function(req, res){
-  setInterval(function(){
-  	var x = 1;
-	res.sendFile('/Users/ling/Documents/eslab/exp2/socketiotest/00'+x+'.jpg'); 
-	x++;
-  }, 1000);
-});//傳了以後不能再傳
-*/
 
 app.get('/d3', function(req, res){
   res.sendFile('/Users/ling/Documents/eslab/exp2/socketiotest/d3.v3.min.js');
@@ -27,15 +19,12 @@ app.get('/socket', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  socket.emit('newphoto');
-  socket.on('photo', function(){
-	var imgname = '/Users/ling/Documents/eslab/exp2/socketiotest/001.jpg';
-	fs.readFile(imgname, 'base64', function(err, img){
-		if (err) throw err;
-		socket.emit('image', {content: 'data:image/jpg;base64,'+img});
+    var imgname = '/Users/ling/Documents/eslab/exp2/socketiotest/001.jpg';    
+    fs.readFile(imgname, 'base64', function(err, img){
+	if (err) throw err;
+	socket.emit('image', {content: 'data:image/jpg;base64,'+img});
 
     });
-  });
 
   var a=100;
   setInterval(
